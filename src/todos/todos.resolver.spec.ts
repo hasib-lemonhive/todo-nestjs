3dc7c82208ab7c77f52257c75ab41096081abeb6
Hasibul Hossain <hasibul@lemonhive.com>
2023-07-11 17:30:08 +0600

    added todo resolver test file

---
+++ src/todos/todos.resolver.spec.ts
@@ -0,0 +1,57 @@
+import { Test } from '@nestjs/testing';
+import { TodosResolver } from './todos.resolver';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { TodosService } from './todos.service';
+import { User } from 'src/user/user.entity';
+import { CreateTodoInput } from './dto/create-todo.input';
+import { Todo } from './todo.entity';
+import { DeleteTodoInput } from './dto/delete-doto.input';
+import { UpdateTodoInput } from './dto/update-todo.input';
+import { UpdateTodoOrderInput } from './dto/update-todo-order.dto';
+
+const mockTodoService = () => ({
+    createTodo: jest.fn(),
+
+    deleteTodo: jest.fn(),
+
+    updateTodo: jest.fn(),
+
+    updateTodoOrder: jest.fn(),
+
+    getAllTodos: jest.fn(),
+
+    getMyTodos: jest.fn()
+});
+
+describe('Todo Resolver', () => {
+    let todoResolver: TodosResolver;
+    let todoService: TodosService;
+
+    beforeEach(async () => {
+        const moduleRef = await Test.createTestingModule({
+            providers: [
+                TodosResolver,
+                {
+                    provide: TodosService,
+                    useFactory: mockTodoService
+                }
+            ],
+
+        }).compile();
+
+        todoResolver = moduleRef.get<TodosResolver>(TodosResolver);
+        todoService = moduleRef.get<TodosService>(TodosService);
+    })
+
+
+    describe('getTodos', () => {
+        it('return list of todos', async () => {
+            const mockTodos = [];
+            todoService.getAllTodos = jest.fn().mockResolvedValue(mockTodos);
+            const result = await todoResolver.getTodos()
+
+            expect(todoService.getAllTodos).toHaveBeenCalled();
+            expect(result).toEqual(mockTodos)
+        })
+    })
+})
